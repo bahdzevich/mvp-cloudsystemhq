5f3dbb220eb1220104fdc4aa7218bac2aa51ab35
Eugene Bogdevich <bogdevich96@gmail.com>
2018-09-20 19:34:18 +0300

    Resolved issue was causing StackOverflowError

--- cs-model/src/main/java/com/cloudsystemhq/model/domain/Customer.java
+++ cs-model/src/main/java/com/cloudsystemhq/model/domain/Customer.java
@@ -1,33 +1,45 @@
 package com.cloudsystemhq.model.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "CUSTOMER")
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "roles")
+@ToString(exclude = "roles")
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String email;
-    private String phone;
-    private String password;
-    private Boolean confirmed;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  private String email;
+  private String phone;
+  private String password;
+  private Boolean confirmed;
 
-    @ManyToMany(
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.REMOVE)
-    @JoinTable(
-            name = "CUSTOMER_ROLE",
-            joinColumns = @JoinColumn(name = "customer_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Set<Role> roles = new HashSet<>();
+  @ManyToMany(
+      fetch = FetchType.LAZY,
+      cascade = CascadeType.REMOVE)
+  @JoinTable(
+      name = "CUSTOMER_ROLE",
+      joinColumns = @JoinColumn(name = "customer_id", referencedColumnName = "id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+  private Set<Role> roles = new HashSet<>();
 }
--- cs-model/src/main/java/com/cloudsystemhq/model/domain/Role.java
+++ cs-model/src/main/java/com/cloudsystemhq/model/domain/Role.java
@@ -1,25 +1,33 @@
 package com.cloudsystemhq.model.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "ROLE")
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "customers")
+@ToString(exclude = "customers")
 public class Role {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
 
-    @JsonIgnore
-    @ManyToMany(mappedBy = "roles")
-    private Set<Customer> customers = new HashSet<>();
+  @JsonIgnore
+  @ManyToMany(mappedBy = "roles")
+  private Set<Customer> customers = new HashSet<>();
 }
