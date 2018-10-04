8937c0428df4e40d9b30928c724054a0ca427f2d
Alexandr <lexsawko@gmail.com>
2018-10-04 20:23:41 +0300

    Additional commit

--- cs-repository/src/main/java/com/cloudsystemhq/repository/ResponseRepository.java
+++ cs-repository/src/main/java/com/cloudsystemhq/repository/ResponseRepository.java
@@ -4,9 +4,10 @@ import com.cloudsystemhq.model.domain.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Long>{
-    Set<Response> findResponsesByQuestionId(Long questionId);
+    Optional<List<Response>> findResponsesByQuestionId(Long questionId);
 }
