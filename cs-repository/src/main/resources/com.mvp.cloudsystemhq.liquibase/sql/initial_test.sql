064333a651a6023b512aa7b9046342950e670028
artur.kulik <artur.kulik.97@mail.ru>
2018-10-15 22:16:21 +0300

    CS-16 Setup Liquibase.Update initial change logs.

--- cs-repository/src/main/resources/com.mvp.cloudsystemhq.liquibase/sql/initial_test.sql
+++ cs-repository/src/main/resources/com.mvp.cloudsystemhq.liquibase/sql/initial_test.sql
@@ -1,5 +1,3 @@
-INSERT INTO TestTable (id, testfiels)
-  1, 'any text 1';
-
-INSERT INTO TestTable (id, testfiels)
-  2, 'any text 2';
+INSERT INTO role (id, name) VALUES (1, 'ADMIN');
+INSERT INTO customer (id, email, password) VALUES (1, 'artur@mail.ru', '{bcrypt}$2a$10$SU.bQp7SWnhJ2uVYfqKqsOZ/yABefiK0V4Ff8CbF2pgPtZhrd0ny6');
+INSERT INTO customer_role (id, role_id, customer_id) VALUES (1, 1, 1);
